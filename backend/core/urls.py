00cfef06b1e2d7138ee8b55de901c9cfe2656920
agatalangova17 <agata.langova@praguecollege.cz>
2025-04-13 13:46:16 +0200

    Modularize views into product_views.py and auth_views.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,18 +1,11 @@
 from django.urls import path
-from .api_views import (
-    best_deal_by_id,
-    all_variants_by_product,
-    list_categories,
-    products_by_category,
-    list_all_products,
-    calculate_basket, 
-)
+from core.views import product_views  
 
 urlpatterns = [
-    path('best-deal/<int:product_id>/', best_deal_by_id),
-    path('all-variants/<int:product_id>/', all_variants_by_product),
-    path('categories/', list_categories),
-    path('products-by-category/<int:category_id>/', products_by_category),
-    path('products/', list_all_products),
-    path('basket/', calculate_basket),
+    path('best-deal/<int:product_id>/', product_views.best_deal_by_id),
+    path('all-variants/<int:product_id>/', product_views.all_variants_by_product),
+    path('categories/', product_views.list_categories),
+    path('products-by-category/<int:category_id>/', product_views.products_by_category),
+    path('products/', product_views.list_all_products),
+    path('basket/', product_views.calculate_basket),
 ]
