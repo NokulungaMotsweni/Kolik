a5c64ebf0336e2b3c07bf50a3b2d9f637bab1440
agatalangova17 <agata.langova@praguecollege.cz>
2025-04-22 22:11:46 +0200

    Move verify endpoint from config/urls.py to users/urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -8,11 +8,12 @@ These endpoints handle:
 """
 
 from django.urls import path
-from users.views import RegisterView, LoginView, LogoutView
+from users.views import RegisterView, LoginView, LogoutView, VerifyUserView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('verify/', VerifyUserView.as_view(), name='verify-user'),
 ]
