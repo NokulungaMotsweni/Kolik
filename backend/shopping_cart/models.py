cdd99cef6b44cf395b0cbdf8f5f7d8ce90ea8a76
Nokulunga (Noki) Motsweni <nokulunga.motsweni@praguecollege.cz>
2025-05-05 10:36:34 +0200

    Add ShoppingCart Model with one-to-one user relationship and timestamps that track the creation and updating of the user's shopping cart.

--- backend/shopping_cart/models.py
+++ backend/shopping_cart/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from products.models import GenericProduct, ProductVariant
+
+
+class ShoppingCart(models.Model):
+    """
+    Represents ab shopping cart that is associated with a single user.
+
+    This models keeps track of the creation and the update times of the cart
+    and establishes a one-to-one relationship with the user.
+    """
+
+    # Default Moel Manager
+    objects = models.Manager()
+
+    # Each user has exactly one shopping cart
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='shopping_cart')
+
+    # Timestamp - When the shopping cart was created
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Timestamp - When the shopping cart was created
+    updated_at = models.DateTimeField(auto_now=True)
 
 # Create your models here.
