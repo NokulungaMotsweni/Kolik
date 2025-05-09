019e982a36297f7806fe56d5d3aa9c7845cf334c
Nokulunga (Noki) Motsweni <nokulunga.motsweni@praguecollege.cz>
2025-05-09 13:00:18 +0200

    Change the name to add Log and to better reflect the function of the class.

--- backend/monitoring/models.py
+++ backend/monitoring/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 
-class GeoAccessAttempt(models.Model):
+class GeoAccessLog(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     ip_address = models.GenericIPAddressField()
     country = models.CharField(max_length=3, null=True, blank=True)
