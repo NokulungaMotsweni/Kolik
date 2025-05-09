a55ce84fc90b43d22d1b97baab93f0c9fe5eec3c
Nokulunga (Noki) Motsweni <nokulunga.motsweni@praguecollege.cz>
2025-05-09 13:08:42 +0200

    Add DocStrings and comments in the GeoAccessLog model.

--- backend/monitoring/models.py
+++ backend/monitoring/models.py
@@ -2,14 +2,39 @@ from django.db import models
 from django.conf import settings
 
 class GeoAccessLog(models.Model):
+    """
+    Stores metadata about access attempts based on geolocation.
+
+    Fields:
+    timestamp (datetime): Timestamp when this access was attempted.
+    ip_address (str): IP address of this access attempt.
+    country (str): Country code for this access attempt.
+    is_proxy (bool): Whether this access attempt is proxied.
+    status (str): Status of this access attempt.
+    path (str): Path of this access attempt.
+    """
+
+    # Log Creation
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    # Client IP Address
     ip_address = models.GenericIPAddressField()
+
+    # Country Code
     country = models.CharField(max_length=3, null=True, blank=True)
+
+    # Whether IP is a proxy or VPN
     is_proxy = models.BooleanField(default=False)
+
+    # Access Status Description
     status = models.CharField(max_length=50)
+
+    # Accessed URL path
     path = models.CharField(max_length=255)
-    
 
     def __str__(self):
+        """
+        Returns a string representation of the access log entry.
+        """
         return f"{self.timestamp} | {self.ip_address} | {self.status}"
 # Create your models here.
