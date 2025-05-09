1e5c7720620161e04e1965f82872fdd010844459
Nokulunga (Noki) Motsweni <nokulunga.motsweni@praguecollege.cz>
2025-05-09 12:24:04 +0200

    Change model name to be more descriptive to its function/role in the project.

--- backend/monitoring/models.py
+++ backend/monitoring/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 
-class Monitoring(models.Model):
+class GeoAccessAttempt(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     ip_address = models.GenericIPAddressField()
     country = models.CharField(max_length=3, null=True, blank=True)
