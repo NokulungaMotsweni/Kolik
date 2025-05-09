52c2ed6f8bfc5ef5e860fd1e91600bd6f5879b79
Nokulunga (Noki) Motsweni <nokulunga.motsweni@praguecollege.cz>
2025-05-09 13:12:11 +0200

    Add DocStrings and comments to the GeoAccessLogAdmin panel. Change the name to reflect the model.

--- backend/monitoring/admin.py
+++ backend/monitoring/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import GeoAccessLog
 
 class GeoAccessAttemptAdmin(admin.ModelAdmin):
-    llist_display = ("timestamp", "ip_address", "country", "is_proxy", "status", "path")
+    list_display = ("timestamp", "ip_address", "country", "is_proxy", "status", "path")
     list_filter = ("status", "country", "is_proxy")
     search_fields = ("ip_address", "path")
+
+    admin.site.register(GeoAccessLog, GeoAccessAttemptAdmin)
 
 # Register your models here.
