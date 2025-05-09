2aa0573b1ed5e5e04bfd6c323a39a0995efdc16b
Nokulunga (Noki) Motsweni <nokulunga.motsweni@praguecollege.cz>
2025-05-09 13:01:46 +0200

    Remove get_user.

--- backend/monitoring/admin.py
+++ backend/monitoring/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import GeoAccessLog
+
+class GeoAccessAttemptAdmin(admin.ModelAdmin):
+    llist_display = ("timestamp", "ip_address", "country", "is_proxy", "status", "path")
+    list_filter = ("status", "country", "is_proxy")
+    search_fields = ("ip_address", "path")
 
 # Register your models here.
